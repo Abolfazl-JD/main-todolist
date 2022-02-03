43f01c8b80d705125b7b2bc22c216a4601a58ca8
abolfazl-hue <abolfazljdst@gmail.com>
2022-02-03 11:58:32 +0330

    add data to state of vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { uuid } from "./utils";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    todoList: [
+      {
+        name: "cooking",
+        completed: false,
+        id: uuid(),
+      },
+      {
+        name: "studying",
+        completed: false,
+        id: uuid(),
+      },
+      {
+        name: "go for a walk",
+        completed: false,
+        id: uuid(),
+      },
+    ],
+  },
   mutations: {},
   actions: {},
   modules: {},
